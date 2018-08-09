54b354bfd2b0c2c262a555086d79a5e050686780
Jennifer <JenniferMack@users.noreply.github.com>
2018-08-09 00:45:51 -0700

    add prob. sums

--- dice_test.go
+++ dice_test.go
@@ -64,13 +64,21 @@ func TestHardWay(t *testing.T) {
 func TestProb(t *testing.T) {
 	var graph = make(map[int]int)
 	sides := 6
+
 	d1, d2 := NewDie(sides), NewDie(sides)
 	for i := 0; i < 10000; i++ {
 		r1, r2 := d1.Roll(), d2.Roll()
 		graph[r1+r2] += 1
 	}
+
+	var sum int
+	var pct float64
+
 	fmt.Println("Results of 10,000 throws")
 	for i := 2; i <= 12; i++ {
+		sum += graph[i]
+		pct += (float64(graph[i]) / float64(10000)) * 100
 		fmt.Printf("%2d : %4d : %5.2f%%\n", i, graph[i], (float64(graph[i])/float64(10000))*100)
 	}
+	fmt.Printf("    %4d :%6.2f%%\n", sum, pct)
 }
